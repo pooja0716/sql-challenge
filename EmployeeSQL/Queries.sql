a827f129da9a209d6007ee5b2c09ac56c2306f5c
Pooja Patel <poojapatel.patel059@gmail.com>
2020-09-29 12:23:23 -0400

    update

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -48,7 +48,7 @@ JOIN departments d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
 
-/* List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name */
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name 
 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
@@ -58,7 +58,7 @@ WHERE d.dept_name = 'Sales'
 OR d.dept_name = 'Development';
 
 
-/* In descending order, list the frequency count of employee last names, i.e., how many employees share each last name */
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name 
 
 SELECT last_name, COUNT(last_name)
 FROM employees
